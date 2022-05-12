798c56e8a591a1bc5062a2bd9705e30007bf3ced
= <=>
2022-05-12 12:46:28 -0300

    Commit inicial da aula 4, devia ter feito esse commmit antes

---
+++ src/main/java/com/example/agenda/ui/activity/ListaAlunosActivity.java
@@ -0,0 +1,28 @@
+package com.example.agenda.ui.activity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.agenda.R;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ListaAlunosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setTitle("Lista de alunos");
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex","Fran","Jose"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,alunos));
+    }
+
+}
